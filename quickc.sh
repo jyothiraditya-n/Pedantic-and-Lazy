d99f38c1152385c7cd1e87bef502e8a0a5b22daa
Jyothiraditya Nellakra <jyothiraditya.n@gmail.com>
2023-01-13 18:13:49 -0600

    Update quickc.sh

--- quickc.sh
+++ quickc.sh
@@ -18,32 +18,63 @@
 checkcmd() { command -v "$1" || { printerr "'$1' not installed."; exit 1; }; }
 printerr() { printf "$0: Error: %s\n" "$*" >&2; }
 
+generate() {
+	if [[ ${1:0:1} == "#" ]]; then
+		echo "$1" >> "/tmp/pal.quickc.h"
+		cat "/tmp/pal.quickc.h" "/tmp/pal.quickc.txt"
+		echo "}" >> "/tmp/pal.quickc.c"
+
+		return
+	fi
+
+	cat "/tmp/pal.quickc.h" "/tmp/pal.quickc.txt"
+	echo "$1" >> "/tmp/pal.quickc.c"
+	echo "}" >> "/tmp/pal.quickc.c"
+}
+
 checkcmd gcc
-checkcmd editor
 
 if ! [ -d "$HOME/.config" ]; then mkdir -p "$HOME/.config" || exit 1; fi
 
-if ! [ -f "$HOME/.config/pal.quickc.c" ]; then
-	touch "$HOME/.config/pal.quickc.c" || exit 1
-	echo "#include <stdio.h>" >> "$HOME/.config/pal.quickc.c"
-	echo "#include <stdlib.h>" >> "$HOME/.config/pal.quickc.c"
-	echo "" >> "$HOME/.config/pal.quickc.c"
-	echo "int main() {" >> "$HOME/.config/pal.quickc.c"
-	echo "	printf(\"Hello, World!\\n\");" >> "$HOME/.config/pal.quickc.c"
-	echo "	exit(0);" >> "$HOME/.config/pal.quickc.c"
-	echo "}" >> "$HOME/.config/pal.quickc.c"
+if ! [ -f "$HOME/.config/pal.quickc.h" ]; then
+	touch "$HOME/.config/pal.quickc.h" || exit 1
+{
+	echo "#include <assert.h>";      echo "#include <limits.h>"
+	echo "#include <signal.h>";      echo "#include <stdlib.h>"
+	echo "#include <ctype.h>";       echo "#include <locale.h>"
+	echo "#include <stdarg.h>";      echo "#include <string.h>"
+	echo "#include <errno.h>";       echo "#include <math.h>"
+	echo "#include <stddef.h>";      echo "#include <time.h>"
+	echo "#include <float.h>";       echo "#include <setjmp.h>"
+	echo "#include <stdio.h>";
+
+	echo "#include <iso646.h>";      echo "#include <wchar.h>"
+	echo "#include <wctype.h>"
+
+	echo "#include <complex.h>";     echo "#include <inttypes.h>"
+	echo "#include <stdint.h>";      echo "#include <tgmath.h>"
+	echo "#include <fenv.h>";        echo "#include <stdbool.h>"
+
+	echo "#include <stdalign.h>";    echo "#include <stdatomic.h>"
+	echo "#include <stdnoreturn.h>"; echo "#include <threads.h>"
+	echo "#include <uchar.h>"
+
+	echo "int main() {"
+}	>> "$HOME/.config/pal.quickc.h"
 fi
 
-if ! [ -f "/tmp/pal.quickc.c" ]; then
-	cp "$HOME/.config/pal.quickc.c" "/tmp/pal.quickc.c" || exit 1
-fi
+cp "$HOME/.config/pal.quickc.h" "/tmp/pal.quickc.h" || exit 1;
+echo "" > "/tmp/pal.quickc.txt" || exit 1
 
-editor "/tmp/pal.quickc.c"; clear
+while true; do
+	read -rp "pal> " command
+	generate "$command" > "/tmp/pal.quickc.c" || exit 1;
 
-while ! gcc -std="gnu99" "/tmp/pal.quickc.c" -o "/tmp/pal.quickc"; do
-	read -p "Hit enter to continue";
-	editor "/tmp/pal.quickc.c"; clear
+	while ! gcc -std="c11" "/tmp/pal.quickc.c" -o "/tmp/pal.quickc" -O0; do
+		read -rp "> " command
+		generate "$command" > "/tmp/pal.quickc.c" || exit 1;
+	done
+
+	echo "$command" >> "/tmp/pal.quickc.txt" || exit 1
+	/tmp/pal.quickc
 done
-
-/tmp/pal.quickc
-exit $?
